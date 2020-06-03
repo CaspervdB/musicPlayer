0772cb41f3ac5c0ba9bed3359ed7b5d4eb3439f1
Sytze van der Gaag <43172319+SytzeVDGaag@users.noreply.github.com>
2020-06-03 11:25:15 +0200

    De MuziekDownload werk

---
+++ WpfAppMusicLibraryTest/MainWindowViewModel.cs
@@ -0,0 +1,22 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfAppMusicLibraryTest
+{
+    class MainWindowViewModel
+    {
+        public ICommand ButtonCommand { get; set; }
+        private VideoLibraryTester vlt;
+
+        public MainWindowViewModel()
+        {
+            this.vlt = new VideoLibraryTester();
+            ButtonCommand = new RelayCommand(async () => await vlt.SaveAudioToDisk2Async());
+        }
+    }
+}
---
+++ WpfAppMusicLibraryTest/VideoLibraryTester.cs
@@ -0,0 +1,56 @@
+﻿using MediaToolkit;
+using MediaToolkit.Model;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using VideoLibrary;
+using YoutubeExplode;
+using YoutubeExplode.Videos.Streams;
+
+namespace WpfAppMusicLibraryTest
+{
+    class VideoLibraryTester : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string link;
+
+        public string Link
+        {
+            get { return link; }
+            set
+            {
+                if (value.Contains("youtube.com/watch?v="))
+                {
+                    link = value;
+                }
+                NotifyPropertyChanged("");
+            }
+        }
+
+        public async Task SaveAudioToDisk2Async()
+        {
+            var source = @"C:\xampp\htdocs\Lora\musicPlayer\WpfAppMusicLibraryTest\test\";
+            var youtube = new YoutubeClient();
+            var url = "https://www.youtube.com/watch?v=4z2yNxfBRM4";
+            var video = await youtube.Videos.GetAsync(url);
+            var title = video.Title;
+            var legalTitle = string.Join("", title.Split(Path.GetInvalidFileNameChars())); // Removes all possible illegal filename characetrs from the title
+            var streamManifest = await youtube.Videos.Streams.GetManifestAsync(url);
+            var streamInfo = streamManifest.GetAudioOnly().WithHighestBitrate();
+            if (streamInfo != null)
+            {
+                // Get the actual stream
+                var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
+                // Download the stream to file
+                await youtube.Videos.Streams.DownloadAsync(streamInfo, $"{source + legalTitle}.mp3");
+            }
+        }
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
