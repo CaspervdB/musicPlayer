d00a5bcf6eabb38fd976ea9ce07c520ed55db9e5
Jochem <jochembrans@gmail.com>
2020-06-18 20:06:41 +0200

    song wordt aan playlist toegevoegd

--- MediaPlayer/MediaPlayer/MusicExport.cs
+++ MediaPlayer/MediaPlayer/MusicExport.cs
@@ -1,13 +1,8 @@
-﻿using AngleSharp;
-using GalaSoft.MvvmLight.Command;
-using MusicPlayer;
+﻿using MusicPlayer;
 using NReco.VideoConverter;
 using System;
-using System.ComponentModel;
 using System.IO;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
-using System.Windows.Input;
 using YoutubeExplode;
 using YoutubeExplode.Videos;
 using YoutubeExplode.Videos.Streams;
@@ -21,24 +16,29 @@ namespace MediaPlayer
             string source = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName, @"music\");
             playList.PlaylistName += @"\"; //playlistname is the foldername
             YoutubeClient youtube = new YoutubeClient();
-                Video video = await youtube.Videos.GetAsync(link);
-                string legalTitle = string.Join("", video.Title.Split(Path.GetInvalidFileNameChars())); // Removes all possible illegal filename characetrs from the title
-                StreamManifest streamManifest = await youtube.Videos.Streams.GetManifestAsync(link);
-                IStreamInfo streamInfo = streamManifest.GetAudioOnly().WithHighestBitrate();
-                if (streamInfo != null)
-                {
+            Video video = await youtube.Videos.GetAsync(link);
+            string legalTitle = string.Join("", video.Title.Split(Path.GetInvalidFileNameChars())); // Removes all possible illegal filename characetrs from the title
+            StreamManifest streamManifest = await youtube.Videos.Streams.GetManifestAsync(link);
+            IStreamInfo streamInfo = streamManifest.GetAudioOnly().WithHighestBitrate();
+            if (streamInfo != null)
+            {
                 // Get the actual stream
                 //var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
                 // Download the stream to file
                 Console.WriteLine("Ik begin nu aan het downloaden jochem en sietze!");
-                    string fileName = $"{source + playList.PlaylistName + legalTitle}";
-                    await youtube.Videos.Streams.DownloadAsync(streamInfo, fileName + ".mp4");
-                    FFMpegConverter ffMpeg = new FFMpegConverter();
-                    ffMpeg.ConvertMedia(fileName + ".mp4", fileName + ".mp3", "mp3");
-                    File.Delete(fileName + ".mp4");
-                    Song newSong = new Song(fileName + ".mp3");
-                    newSong.ArtistName = video.Author;
-                    newSong.SongTitle = video.Title;
+                string fileName = $"{source + playList.PlaylistName + legalTitle}";
+                await youtube.Videos.Streams.DownloadAsync(streamInfo, fileName + ".mp4");
+                FFMpegConverter ffMpeg = new FFMpegConverter();
+                ffMpeg.ConvertMedia(fileName + ".mp4", fileName + ".mp3", "mp3");
+                File.Delete(fileName + ".mp4");
+                Song newSong = new Song(fileName + ".mp3");
+                newSong.ArtistName = video.Author;
+                newSong.SongTitle = video.Title;
+                playList.addSong(newSong);
+                
+                //TODO
+                //toevoegen aan database
+
             }
 
         }
